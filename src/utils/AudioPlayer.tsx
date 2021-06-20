4fe57ba95f156d9be409e7d89033052c2ccc9f3a
Dolosolow <Jose.Munoz07c@gmail.com>
2021-06-20 18:41:04 -0400

    Refactored the class AudioPlayer to work as a singleton, removing the previous issue with the player running tasks.

--- src/utils/AudioPlayer.tsx
+++ src/utils/AudioPlayer.tsx
@@ -2,12 +2,23 @@ import { Audio } from "expo-av";
 import moment from "moment";
 
 export class AudioPlayer {
+  private static _instance: AudioPlayer = new AudioPlayer();
+
   audioPlayer: Audio.Sound;
 
   constructor() {
-    this.initializePlayer();
-    this.audioPlayer = new Audio.Sound();
+    if (AudioPlayer._instance) {
+      throw new Error("Error: Use AudioPlayer.getInstance() instead of new");
+    } else {
+      this.initializePlayer();
+      AudioPlayer._instance = this;
+      this.audioPlayer = new Audio.Sound();
+    }
   }
+
+  static getInstance = () => {
+    return AudioPlayer._instance;
+  };
 
   private initializePlayer = async () => {
     try {
@@ -18,39 +29,41 @@ export class AudioPlayer {
   };
 
   static formatTimeDuration = (millisec: number) => {
-    return `${moment(millisec).format("m:ss")}`;
+    if (millisec) {
+      return `${moment(millisec).format("m:ss")}`;
+    }
+    return `0:00`;
   };
 
   loadAudio = async (audio: NodeRequire | any) => {
-    await this.audioPlayer.loadAsync(audio, { shouldPlay: false });
+    try {
+      await this.audioPlayer.loadAsync(audio, { shouldPlay: false, positionMillis: 0 });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   pauseAudio = async () => {
     try {
-      await this.audioPlayer.pauseAsync();
-      return await this.getAudioPosition();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  playAudio = async (audio: NodeRequire | any) => {
-    try {
-      if (!this.audioPlayer._loaded) {
-        await this.audioPlayer.loadAsync(audio, { shouldPlay: false });
+      if (this.audioPlayer._loaded) {
+        await this.audioPlayer.pauseAsync();
+        return await this.getAudioPosition();
       }
-      await this.audioPlayer.playAsync();
     } catch (error) {
       console.log(error);
     }
   };
 
-  playAudioAfterPause = async () => {
-    const startPosition = await this.getAudioPosition();
-
+  playAudio = async (resume?: boolean) => {
     try {
-      await this.audioPlayer.setPositionAsync(startPosition);
-      await this.audioPlayer.playAsync();
+      if (resume) {
+        const startPosition = await this.getAudioPosition();
+        await this.audioPlayer.setPositionAsync(startPosition);
+      }
+
+      if (this.audioPlayer._loaded) {
+        await this.audioPlayer.playAsync();
+      }
     } catch (error) {
       console.log(error);
     }
@@ -58,11 +71,14 @@ export class AudioPlayer {
 
   playNewAudio = async (audio: NodeRequire | any) => {
     try {
-      await this.audioPlayer.unloadAsync();
-      if (!this.audioPlayer._loaded || !this.audioPlayer._loading) {
-        await this.audioPlayer.loadAsync(audio);
+      if (this.audioPlayer._loaded) {
+        await this.audioPlayer.stopAsync();
+        await this.audioPlayer.unloadAsync();
       }
-      await this.audioPlayer.playAsync();
+
+      if (!this.audioPlayer._loaded || !this.audioPlayer._loading) {
+        await this.audioPlayer.loadAsync(audio, { shouldPlay: true, positionMillis: 0 });
+      }
     } catch (error) {
       console.log(error);
     }
@@ -70,21 +86,48 @@ export class AudioPlayer {
 
   setAudioPosition = async (position: number = 0) => {
     try {
-      await this.pauseAudio();
       await this.audioPlayer.setPositionAsync(position);
-      await this.audioPlayer.playAsync();
     } catch (error) {
       console.log(error);
     }
   };
 
   getAudioPosition = async () => {
-    const results: any = await this.audioPlayer.getStatusAsync();
-    return results.positionMillis;
+    try {
+      let results: any;
+
+      if (this.audioPlayer._loaded) {
+        results = await this.audioPlayer.getStatusAsync();
+      }
+
+      return results.positionMillis;
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   getAudioDuration = async () => {
-    const results: any = await this.audioPlayer.getStatusAsync();
-    return results.durationMillis;
+    try {
+      let results: any;
+
+      if (this.audioPlayer._loaded) {
+        results = await this.audioPlayer.getStatusAsync();
+      }
+
+      return results.durationMillis;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  getFullAudioProgress = async () => {
+    if (this.audioPlayer._loaded) {
+      const audioSeek: any = await this.audioPlayer.getStatusAsync();
+
+      return {
+        audioPosition: audioSeek.positionMillis,
+        audioDuration: audioSeek.durationMillis,
+      };
+    }
   };
 }
